d1db8638e88092601684b0f2cf3d80c720d576f6
mojibriilali12-coder <mojibriilali12@gmail.com>
2026-06-09 19:31:10 -0500

    USEEFFECT EXER6

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import ShoppingCart from "./ShoppingCart";
+// import ShoppingCart from "./ShoppingCart";
 
+import { useEffect, useState } from "react";
 
 function App(){
-    return(
-        <>
-         
-         <ShoppingCart/>
+    
+    const[title, setTitle]=useState("hello")
+
+     const[name, setName]=useState("")
+    
+    useEffect(()=>{
+        document.title = name ? `${title} ${name}` : "Welcome";
         
-        </>
+     
+    },[title,name])
+
+
+     
+
+    return(
+       <div>
+        <h2>Type this</h2>
+         <input type="text"  value={name} onChange={(e) =>  setName(e.target.value)} />
+        <input type="text"  value={title} onChange={(e) => setTitle(e.target.value)} />
+       </div>
     )
 }
 
