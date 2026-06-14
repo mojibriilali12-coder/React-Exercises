07087f3cf2ecb88603ccd85c519dcf9285828725
mojibriilali12-coder <mojibriilali12@gmail.com>
2026-06-13 19:55:18 -0500

    STOPWATCH PROJECT

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,49 @@ import { useEffect, useState } from "react";
 
 function App(){
     
-    const[width, setWidth]=useState(window.innerWidth)
+    const[time, setTime]=useState(0)
+
+    const[isRuning, setIsruning]=useState(false)
 
   
-    
     useEffect(()=>{
-        
-       const handleResize= ()=> setWidth(window.innerWidth)
+
+      let timerId;
+      if(isRuning){
+
+        timerId= setInterval(()=>{
+            setTime((prev)=> prev + 1)
+        },1000)
+      }
+
+     return ()=> clearInterval(timerId)
+
+    },[isRuning])
+    
+   
+    const handleStart=()=>{
+      setIsruning(true)
+    }
 
 
-        window.addEventListener('resize', handleResize)
+    const handleStop=()=>{
+      setIsruning(false)
+    }
 
-        return()=>{
-         
-            window.removeEventListener("resize", handleResize)
-        }
-     
-    },[])
+    
+    const handleReset=()=>{
 
-
+      setIsruning(false)
+      setTime(0)
+    }
      
 
     return(
        <div>
-        <p>window width {width}px</p>
+        <h2>StopWatch {time}</h2>
+       <button disabled={isRuning} onClick={handleStart}>Start</button>
+      <button disabled={!isRuning}onClick={handleStop}>stop</button>
+        <button onClick={handleReset}>Reset</button>
        </div>
     )
 }
