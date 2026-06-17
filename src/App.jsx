098c641c8e16085d6b7c1a60fef035670e0861d0
mojibriilali12-coder <mojibriilali12@gmail.com>
2026-06-16 19:09:52 -0500

    REDERING REACT

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,27 @@
 // import ShoppingCart from "./ShoppingCart";
 
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 function App(){
     
-    const[user, setUser]=useState()
+   const todos =[
 
-    const[loading, setLoud]=useState(null)
+    {id:1,text:"Learn React"},
+    {id:1,text:"Build a Project"},
 
-    
-   
 
-  
-      const getUser = async()=>{
-    
-            setLoud(true)
 
-         try{
-     
-            const response = await fetch( "https://api.github.com/users/mojibriilali12-coder")
 
-            const data = await response.json()
-             
-            console.log(data)
-            setUser(data)
-            setLoud(false)
-         }catch(error){
-            console.log('error', error)
-         }
-      }
-
-     
-
-   
-  
-   
-     if(loading) return <h1>Loading...</h1>
+   ];
     
     return(
-      <div>
-         <input type="text" placeholder="github User" onChange={(e)=> setUser(e.target.value)}/>
-         <button onClick={getUser}>Search</button>
-       {user && (
-        <div>
-          <img
-            src={user.avatar_url}
-            alt={user.login}
-            width="150"
-          />
-
-          <h2>{user.name}</h2>
-          <p>Username: {user.login}</p>
-          <p>Followers: {user.followers}</p>
-          <p>Following: {user.following}</p>
-          <p>Public Repos: {user.public_repos}</p>
-
-          <a
-            href={user.html_url}
-            target="_blank"
-            rel="noreferrer"
-          >
-            View Profile
-          </a>
-        </div>
-      )}
-    </div>
+      <ul>
+      {
+       todos.map((todo, index) =>(
+          <li key={todo.id}>{todo.text}</li>
+        ))
+      } 
+      </ul>
   );
 }
 export default App;
